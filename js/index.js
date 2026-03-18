07ddd2034ead3272cf9b75b72c6dc4daa9785e20
cbnuLeehyunwoo <henow123@naver.com>
2026-03-18 18:40:21 +0900

    feat: TIL 사이트 미션 구현

--- js/index.js
+++ js/index.js
@@ -1,12 +1,52 @@
-// TODO: TIL 폼 등록 기능을 구현하세요
-// 1. 폼 요소와 목록 요소를 querySelector로 선택합니다.
-// 2. 폼의 submit 이벤트를 감지하여 새 TIL 항목을 목록에 추가합니다.
+document.addEventListener('DOMContentLoaded', () => {
+  const tilForm = document.getElementById('til-form');
+  const tilList = document.getElementById('til-list');
 
-const tilForm = document.querySelector("#til-form");
-const tilList = document.querySelector("#til-list");
+  loadTils();
 
-tilForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+  tilForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
 
-  // TODO: 입력값을 가져와서 새 TIL 항목을 만들어 목록에 추가하세요
+    const date = document.querySelector('#til-date').value;
+    const title = document.querySelector('#til-title').value;
+    const content = document.querySelector('#til-content').value;
+
+    const newTil = {
+      id: Date.now(),
+      date,
+      title,
+      content
+    };
+
+    addTilToDOM(newTil);
+    saveTilToLocalStorage(newTil);
+
+    tilForm.reset();
+  });
+
+  function addTilToDOM(til) {
+    const article = document.createElement('article');
+    article.className = 'til-item';
+    
+    const formattedContent = til.content.replace(/\n/g, '<br>');
+
+    article.innerHTML = `
+      <time>${til.date}</time>
+      <h3>${til.title}</h3>
+      <p>${formattedContent}</p>
+    `;
+    
+    tilList.prepend(article);
+  }
+
+  function saveTilToLocalStorage(til) {
+    const tils = JSON.parse(localStorage.getItem('myTils')) || [];
+    tils.push(til);
+    localStorage.setItem('myTils', JSON.stringify(tils));
+  }
+
+  function loadTils() {
+    const tils = JSON.parse(localStorage.getItem('myTils')) || [];
+    tils.forEach(til => addTilToDOM(til));
+  }
 });
